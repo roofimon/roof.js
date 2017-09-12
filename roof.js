67c9dcbc4fbd26ba6efbdcf86b1dc88a463d0d49
Twin Panichsombat <roofimon@gmail.com>
2017-09-12 15:44:00 +0700

    Add first cli roof

--- roof.js
+++ roof.js
@@ -1,13 +1,23 @@
-'use strict';
-
-var warning = [
-    '********************************************************************************',
-    '                                     WARNING                                    ',
-    '----------------------Please stop writing javascript----------------------------',
-    '********************************************************************************'
-  ].join('\n');
-  
-console.error(warning);
+#!/usr/bin/env node
+var chalk       = require('chalk');
+var clear       = require('clear');
+var CLI         = require('clui');
+var figlet      = require('figlet');
+var inquirer    = require('inquirer');
+var Preferences = require('preferences');
+var Spinner     = CLI.Spinner;
+var GitHubApi   = require('github');
+var _           = require('lodash');
+var git         = require('simple-git')();
+var touch       = require('touch');
+var fs          = require('fs');
+var files = require('./lib/files');
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('ROOF!@#$%^', { horizontalLayout: 'full' })
+  )
+);
 
 
 var Roof = {};                    
